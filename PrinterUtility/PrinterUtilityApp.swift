2a5f3e7e4a9d42531373ba011e853e7f7e46ecc5
Tahir Anil Oghan <t.anil.oghan@gmail.com>
2025-07-07 22:26:21 +0300

    Initial Commit

---
+++ PrinterUtility/PrinterUtilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrinterUtilityApp.swift
+//  PrinterUtility
+//
+//  Created by Tahir Anil Oghan on 7.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct PrinterUtilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
